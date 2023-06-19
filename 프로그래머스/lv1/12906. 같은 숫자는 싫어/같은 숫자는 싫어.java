667f53fa686c8136ae61ad5086ef00aef430e1db
Jinyoung Choi <jy_3026@naver.com>
2023-06-20 01:17:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 34.51 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        
+        List<Integer> list = new ArrayList<>();
+        
+        list.add(arr[0]);
+        int prevnum = arr[0];
+        
+        for(int i=1;i<arr.length;i++){
+            if(!(arr[i] == prevnum)) {
+                list.add(arr[i]);
+            }
+            prevnum = arr[i];
+        }
+        
+        int[] answer = list.stream().mapToInt(i->i).toArray();
+        
+
+        return answer;
+    }
+}
