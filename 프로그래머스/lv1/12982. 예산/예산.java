4e9651cef8ef72fe5cd8a138f68cc6855fd077ca
Jinyoung Choi <jy_3026@naver.com>
2023-06-20 21:20:22 +0900

    [level 1] Title: 예산, Time: 0.51 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        int number = 0;
+        Arrays.sort(d);
+        
+        for(int num : d){
+            number += num;
+            
+            if(number > budget) return answer;
+            
+            answer++;
+        
+        }
+
+        
+        return answer;
+    }
+}
