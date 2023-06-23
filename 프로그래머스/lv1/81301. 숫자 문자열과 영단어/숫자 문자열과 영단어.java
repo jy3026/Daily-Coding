9f4adc0ae8e89d85a9984b6fc9047dbd410e1a45
Jinyoung Choi <jy_3026@naver.com>
2023-06-23 21:52:41 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.32 ms, Memory: 74.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        
+        Map<String,String> map = new HashMap<>();
+        
+        map.put("0","zero");
+        map.put("1","one");
+        map.put("2","two");
+        map.put("3","three");
+        map.put("4","four");
+        map.put("5","five");
+        map.put("6","six");
+        map.put("7","seven");
+        map.put("8","eight");
+        map.put("9","nine");
+        
+        for(int i =0;i<map.size();i++){
+            s = s.replaceAll(map.get(String.valueOf(i)),String.valueOf(i));
+        }
+        
+        
+        return Integer.parseInt(s);
+    }
+}
