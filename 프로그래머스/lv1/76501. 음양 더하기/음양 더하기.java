3d1a139d48fe1d9c0ac04c03d7248a52ed02e02d
Jinyoung Choi <jy_3026@naver.com>
2023-08-15 20:37:50 +0900

    [level 1] Title: 음양 더하기, Time: 0.04 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -2,9 +2,9 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         
-        for(int i=0;i<absolutes.length;i++){
-            if(signs[i] == true) answer+= absolutes[i];
-            else answer+= absolutes[i]*-1;
+        for(int i=0;i<signs.length;i++){
+            if(signs[i] == true) answer += absolutes[i];
+            else answer += absolutes[i]*-1;
         }
         
         return answer;
