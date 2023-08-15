5db92954a697b80e8e3b7d0dd087a6ab7e06e7ff
Jinyoung Choi <jy_3026@naver.com>
2023-08-15 20:48:14 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 2.20 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,19 +1,15 @@
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(int[] numbers) {
         int answer = 0;
         
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list = Arrays.stream(numbers).boxed().collect(Collectors.toList());
         
-        for(int i=0;i<numbers.length;i++){
-            list.add(numbers[i]);
+        for(int i=0;i<10;i++){
+            if(!list.contains(i)) answer+=i;
         }
-        
-        for(int i=1;i<10;i++){
-            if(!list.contains(i)) answer+= i;
-        }
-        
         
         return answer;
     }
