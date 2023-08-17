f89c8c38173b94dbd84f50bd82a6aca3e151584f
Jinyoung Choi <jy_3026@naver.com>
2023-08-17 20:15:45 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.06 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,16 +1,14 @@
 class Solution {
     public boolean solution(String s) {
-        boolean answer = false;
-        
-        if(s.length()==4 || s.length()==6){
-            for(char c : s.toCharArray()){
-                if(!Character.isDigit(c)) return false;
+        if(s.length() == 4 || s.length() == 6){
+            try{
+                Long.parseLong(s);
+                return true;
+            }catch(Exception e){
+                return false;
             }
-            
-            return true;
-            
         }
         
-        return answer;
+        return false;
     }
 }
