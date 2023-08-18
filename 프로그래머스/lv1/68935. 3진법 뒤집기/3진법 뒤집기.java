36f9bc0f8d04229db0e91b31f6b6f1c7ac753c53
Jinyoung Choi <jy_3026@naver.com>
2023-08-18 20:40:28 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -3,33 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String num = "";
-        List<Integer> list  = new ArrayList<>();
+        String three = Integer.toString(n,3);
         
-        while(n>0){
-            list.add(n%3);
-            n = n/3;
-        }
+        StringBuilder sb = new StringBuilder(three);
         
-        for(int i=list.size()-1;i>=0;i--){
-            num += String.valueOf(list.get(i));
-        }
+        String result = sb.reverse().toString();
         
-        String[] arr = num.split("");
-        
-        
-        for(int i=0;i<arr.length;i++){
-            if(i==0){
-                answer += Integer.parseInt(arr[i]) * 1;
-            }else{
-                answer += Integer.parseInt(arr[i]) * Math.pow(3,i);
-            }
-            
-        }
-        
-        
-        
-        System.out.println(num);
+        answer = Integer.parseInt(result,3);
         
         return answer;
     }
