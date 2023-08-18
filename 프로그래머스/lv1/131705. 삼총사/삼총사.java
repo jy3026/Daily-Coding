dacafd2b983f579beedd2a80fd323d67b2c6f04e
Jinyoung Choi <jy_3026@naver.com>
2023-08-18 21:14:20 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.java
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -1,25 +1,14 @@
-import java.util.*;
-
 class Solution {
-    private int count = 0;
-
     public int solution(int[] number) {
-        backtrack(number, new ArrayList<>(), 0, 0);
-        return count;
-    }
-
-    private void backtrack(int[] number, List<Integer> selected, int start, int sum) {
-        if (selected.size() == 3) {
-            if (sum == 0) {
-                count++;
+        int count = 0;
+        for(int i=0; i<number.length-2;i++){
+            for(int j=i+1; j<number.length-1;j++){
+                for(int k=j+1; k<number.length;k++){
+                    if(number[i]+number[j]+number[k]==0) count++;
+                }
             }
-            return;
         }
-
-        for (int i = start; i < number.length; i++) {
-            selected.add(number[i]);
-            backtrack(number, selected, i + 1, sum + number[i]);
-            selected.remove(selected.size() - 1);
-        }
+        
+        return count;
     }
 }
