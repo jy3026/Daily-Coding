be0d9681e5eba0d6d5c6c000bdc58172f3d36aa5
Jinyoung Choi <jy_3026@naver.com>
2023-08-20 20:48:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 2.76 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,38 +1,32 @@
 class Solution {
-    public String[] addZero(String bin,int n){
-        if(bin.length() != n) {
-            String zero = "";
-            for(int i=0; i<n-bin.length(); i++) {
-                zero += "0";
-            }
-            bin = zero + bin;
-        }
-
-        return bin.split("");
+    public String change(String num){
+        String ch ="";
         
+        for(int i=0;i<num.length();i++){
+            if(num.charAt(i) == '1'){
+                ch += '#';
+            }else if(num.charAt(i) == '0'){
+                ch += ' ';
+            }
+        }
+        
+        return ch;
     }
     
-    
-    
     public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = new String[n];
+        String[] answer = new String[arr1.length];
         
-        for(int i =0;i<n;i++){
-            String a1 = Integer.toBinaryString(arr1[i]);
-            String a2 = Integer.toBinaryString(arr2[i]);
+        for(int i=0;i<arr1.length;i++){
+            String num = Integer.toBinaryString(arr1[i] | arr2[i]);
             
-            String[] num1 = addZero(a1,n);
-            String[] num2 = addZero(a2,n);
-            
-            String plus = "";
-            
-            for(int j=0;j<n;j++){
-                if(num1[j].equals("1") || num2[j].equals("1")) plus += "#";
-                else plus += " ";
+            if(num.length() != arr1.length){
+                String space = "0";
+                for(int j=0;j<=(arr1.length-num.length());j++){
+                    num = space + num;
+                }
             }
             
-            answer[i] = plus;
-            
+            answer[i] = change(num);
         }
         
         return answer;
