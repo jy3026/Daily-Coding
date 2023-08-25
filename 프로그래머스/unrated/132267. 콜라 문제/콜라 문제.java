3dc7e99a756d758c9e823709b496ccbf810fd90a
Jinyoung Choi <jy_3026@naver.com>
2023-08-25 23:12:43 +0900

    [unrated] Title: 콜라 문제, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -1,17 +1,10 @@
 class Solution {
     public int solution(int a, int b, int n) {
-    int answer = 0;
-    int emptyBottles = n;
-
-    while (emptyBottles >= a) {
-        // 새로 받은 콜라 병 수를 구하고 누적
-        int newBottles = emptyBottles / a * b;
-        answer += newBottles;
-        
-        // 남은 빈 병 개수 계산
-        emptyBottles = emptyBottles % a + newBottles;
-    }
-
-    return answer;
+        int answer = 0;
+        while (n >= a){
+            n = n - (a-b);
+            answer += b;
+        }
+        return answer;
     }
 }
