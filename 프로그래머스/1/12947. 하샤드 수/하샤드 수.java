ef28b094d9701938a4595a079403e3ca0287d9cb
Jinyoung Choi <jy_3026@naver.com>
2023-12-25 19:08:18 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean solution(int x) {
+        int number = x;
+        int num = 0;
+        
+        while(x>0){
+            num += x%10;
+            x = x/10;
+        }
+        
+        if(number%num==0) return true;
+        return false;
+
+    }
+}
