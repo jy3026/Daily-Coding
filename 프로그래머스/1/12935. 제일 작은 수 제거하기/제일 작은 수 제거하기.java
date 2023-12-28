bd637e287667b01ecdf7cfe8abddd9bc0636bb54
Jinyoung Choi <jy_3026@naver.com>
2023-12-28 21:48:48 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.66 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int[] answer = new int[1];
+        if(arr.length == 1) return answer = new int[]{-1};
+        
+        int min = Integer.MAX_VALUE;
+        
+        for(int num : arr){
+            if(min > num) min = num;
+        }
+        
+        List<Integer> list = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i] != min) list.add(arr[i]);
+        }
+        
+        int[] result = list.stream().mapToInt(i->i).toArray();
+        
+        return result;
+    }
+}
