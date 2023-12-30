01b3c7bd92a4655eda14202dc27336347de9e7a1
Jinyoung Choi <jy_3026@naver.com>
2023-12-30 23:01:18 +0900

    [level 1] Title: 평균 구하기, Time: 0.02 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -1,9 +1,11 @@
 class Solution {
     public double solution(int[] arr) {
         double result = 0;
+        
         for(int num : arr) {
             result += num;
         }
+        
         return result / arr.length;
     }
 }
